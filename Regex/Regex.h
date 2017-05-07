cf9617f92836f015bdf83e18dd7e639988470d3d
devedbox <862099730@qq.com>
2017-05-07 16:34:09 +0800

    Updated.

---
+++ Regex/Regex.h
@@ -0,0 +1,19 @@
+//
+//  Regex.h
+//  Regex
+//
+//  Created by devedbox on 2017/5/6.
+//  Copyright © 2017年 jiangyou. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Regex.
+FOUNDATION_EXPORT double RegexVersionNumber;
+
+//! Project version string for Regex.
+FOUNDATION_EXPORT const unsigned char RegexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Regex/PublicHeader.h>
+
+
